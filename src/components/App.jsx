e965894b84ba1eb90760af475a4dd9785ded4c60
AleksandrMaslov <amaslov8910@gmail.com>
2023-02-14 17:52:43 +0300

    add loaders babel

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './app.less'
+
+export function App() {
+  return (
+    <div className='app'>
+      REACT IS WORKING!
+    </div>
+  )
+}
--- webpack.config.js
+++ webpack.config.js
@@ -4,13 +4,16 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: ['@babel/polyfill', './src/index.jsx'],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].[hash].js',
   },
   devServer: {
     port: 3000,
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
   },
   plugins: [
     new HtmlWebpackPlugin({ template: './src/index.html' }),
@@ -25,6 +28,30 @@ module.exports = {
       {
         test: /\.(jpg|jpeg|png|svg)/,
         use: ['file-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        ],
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-react', '@babel/preset-env'],
+            },
+          },
+        ],
       },
     ],
   },
